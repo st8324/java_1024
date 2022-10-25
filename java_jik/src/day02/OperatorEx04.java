895a1de9473d6e8fa211b27633258760e1468706
stajun <stajun@naver.com>
2022-10-25 21:08:14 +0900

    자바 2일차 : 비교 연산자 예제

---
+++ java_jik/src/day02/OperatorEx04.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class OperatorEx04 {
+
+	public static void main(String[] args) {
+		/* 비교 연산자 : 크기 비교
+		 *  - 결과값이 참 또는 거짓이 나옴
+		 * 조건식 : 연산 결과가 참 또는 거짓이 나오는 식
+		 * */
+		int age = 17; 
+		System.out.println(age + "살은 성인인가? " + (age >= 19));
+		int score = 80;
+		System.out.println(score + "점은 A학점인가? " + (score >= 90));
+	}
+
+}
