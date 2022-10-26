e7978c826e2b3961da173e9f2ff47e626abb711b
stajun <stajun@naver.com>
2022-10-26 21:35:56 +0900

    자바 03일차 : for문 예제 - 1부터 5까지 출력 예제

---
+++ java_jik/src/day03/ForEx02.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class ForEx02 {
+
+	public static void main(String[] args) {
+		// 1부터 5까지 출력하는 코드를 for문을 이용하여 작성하세요.
+		/* 반복횟수 : i는 1부터 5까지 1씩 증가 
+		 * 규칙성 : i를 출력
+		 * */
+		int i;
+		for( i = 1 ; i <= 5 ; i++ ) {
+			System.out.println(i);
+		}
+	}
+
+}
