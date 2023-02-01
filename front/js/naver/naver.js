1655c742c8111b96dfc567e942eae98510f8df64
stajun <stajun@naver.com>
2023-02-01 17:51:51 +0900

    JS 10일차 - jquery - 네이버 왼쪽 2번째 컨텐츠 롤링 적용

---
+++ front/js/naver/naver.js
@@ -0,0 +1,59 @@
+$(function(){
+	$('.box-body-right2 .btn-next').click(function(e){
+		e.preventDefault();
+		moveLeft(liRight2, ulRight2, timeRight2);
+	});
+	
+	$('.box-body-right2 .btn-prev').click(function(e){
+		e.preventDefault();
+		moveRight(liRight2, ulRight2, timeRight2);
+	});
+	
+	
+	$('.container').hover(function(){
+		clearInterval(id);
+	}, function(){
+		id = rolling();
+	});
+});
+
+let liRight2 = '.box-body-right2 .item-stock';
+let ulRight2 = '.box-body-right2 .list-stock';
+let timeRight2 = 1000;
+let durationRight2 = 2000;
+
+let rollingObjRigh2 = {
+	li : '.box-body-right2 .item-stock',
+ 	ul : '.box-body-right2 .list-stock',
+	time : 1000,
+	duration : 2000
+}
+
+let rollingRight2 = rollingLeft(rollingObjRigh2.li, rollingObjRigh2.ul, rollingObjRigh2.time, rollingObjRigh2.duration);
+
+function moveLeft(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector).first().animate({
+			marginLeft : `-${width}px`
+		},time, function(){
+			$(this).detach().appendTo(ulSelector).removeAttr('style');
+		});
+	}
+}
+function moveRight(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector)
+			.last()
+			.detach()
+			.prependTo(ulSelector)
+			.css('marginLeft',`-${width}px`)
+			.animate({
+				marginLeft : 0
+			}, time);
+	}
+}
+function rollingLeft(liSelector, ulSelector, animationTime, duration){
+	return setInterval(moveLeft,duration,liSelector, ulSelector, animationTime);
+}
