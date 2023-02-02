784356c21a53597e213c0b29a8d91a086d8a8073
stajun <stajun@naver.com>
2023-02-02 19:20:44 +0900

    JS 11일차 - jquery - 네이버 왼쪽 2번째 컨텐츠 버튼 기능 구현

--- front/js/naver/naver.js
+++ front/js/naver/naver.js
@@ -44,7 +44,42 @@ $(function(){
 		boxShopIn.find('.current-page').text(curPage);
 	});
 });
+//왼쪽 두번째 뉴스 리스트 버튼 관리
+$(function(){
 
+	selectNewsList($('.box-body-left2 .list-press').eq(3))
+
+	$('.box-body-left2 .btn-prev').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left2 .list-press.selected');
+		if(obj.prev().length != 0){
+			selectNewsList(obj.prev());
+		}
+	});
+	$('.box-body-left2 .btn-next').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left2 .list-press.selected');
+		if(obj.next().length == 0 || !obj.next().hasClass('list-press'))
+			return;
+		selectNewsList(obj.next());
+	});
+});
+
+function selectNewsList(el){
+	$('.box-body-left2 .list-press').removeClass('selected').hide();
+	el.show();
+	el.addClass('selected')
+	if(el.prev().length == 0){
+		$('.box-body-left2 .btn-prev').hide();
+	}else{
+		$('.box-body-left2 .btn-prev').show();
+	}
+	if(el.next().length == 0 || !el.next().hasClass('list-press')){
+		$('.box-body-left2 .btn-next').hide();
+	}else{
+		$('.box-body-left2 .btn-next').show();
+	}
+}
 
 let liRight2 = '.box-body-right2 .item-stock';
 let ulRight2 = '.box-body-right2 .list-stock';
