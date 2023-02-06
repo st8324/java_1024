02e71a6e1e914140dcbfa0ff320e6c43dd00d22e
stajun <stajun@naver.com>
2023-02-06 19:51:11 +0900

    JS 13일차 - jquery - 네이버 메뉴 더보기 기능 구현 중

--- front/js/naver/naver.js
+++ front/js/naver/naver.js
@@ -119,7 +119,53 @@ $(function(){
 		e.preventDefault();
 		setMenuServiceBtn();
 	})
-})
+	$('.group-service-check [type=checkbox]').click(function(){
+		//클릭한 요소의 value를 가져옴
+		let val = $(this).val();
+
+		//전역 배열에 val이 있는지 확인 (또는 클릭한 요소의 checked 여부를 확인)
+		let index = tmpMenuArr.indexOf(val);
+		//배열에 val가 있으면 (또는 클릭한 요소가 checked가 해제되면)
+		if(index >= 0){
+			//배열에 val를 제거
+			tmpMenuArr.splice(index, 1);//index 번지부터 1개 삭제
+		}
+		//없으면
+		else{
+			//배열의 길이가 4이상이면 현재 선택한 요소를 checked를 해제하고 알림창을 띄운 후 종료
+			if(tmpMenuArr.length >= 4){
+				$(this).prop('checked', false);
+				alert('최대 4개까지 설정할 수 있습니다.');
+				return;
+			}
+			//배열에 val를 추가
+			tmpMenuArr.push(val);
+		}
+		//주어진 전역 배열을 기준으로 list-empty-box 요소를 배치(함수로 따로 만듬)
+		drawEmptyboxMenu(tmpMenuArr);
+	});
+	//저장 버튼
+	$('.group-menu .btn-save').click(function(){
+		selectedMenuArr = tmpMenuArr.splice(0);
+		init();
+	});
+});
+//선택된 메뉴에 따른 메뉴 박스 관리 및 체크박스 관리하는 함수
+function init(){
+
+}
+function drawEmptyboxMenu(tmpMenuArr){
+	//select : 녹색박스, select 클래스를 제거=>녹색박스 제거
+	//모든 박스에 있는 글자들을 ''(빈 문자열)로 초기화
+	$('.list-empty-box .item-box').removeClass('select').text('');
+	for(index in tmpMenuArr){
+		$('.list-empty-box .item-box').eq(index).text(tmpMenuArr[index]);
+	}
+	
+	$('.list-empty-box .item-box').eq(tmpMenuArr.length).addClass('select');
+}
+let tmpMenuArr = [];  //선택한 메뉴를 저장할 임시 배열(저장 전)
+let selectedMenuArr = []; //선택한 메뉴를 저장할 배열(저장 완료)
 function setMenuServiceBtn(flag){
 	$('.group-menu .box-btn-area .btn').removeClass('display-none');
 	$('.container-service .group-service').removeClass('display-none');
