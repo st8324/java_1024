6c33ab275c509d01e12ee8dc13c2f4024d7bea6c
stajun <stajun@naver.com>
2023-02-27 19:53:39 +0900

    스프링 14일차 : 게시글 파일 업로드 구현 완료

--- spring project/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
+++ spring project/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
@@ -22,6 +22,43 @@ public class BoardServiceImp implements BoardService {
 	
 	String uploadPath = "D:\\uploadfiles";
 
+	private boolean checkBoard(BoardVO board) {
+		//게시글이 없거나, 게시글 제목이 비어있거나, 내용이 비어있으면
+		if(board == null || 
+			board.getBo_title() == null|| 
+			board.getBo_title().trim().length() == 0 ||
+			board.getBo_content() == null || 
+			board.getBo_content().trim().length() == 0)
+			return false;
+		
+		return true;
+	}
+	
+	private void uploadFiles(MultipartFile [] files, int bo_num) {
+		if(files == null || files.length == 0)
+			return ;
+		//반복문
+		for(MultipartFile file : files) {
+			if(file == null || file.getOriginalFilename().length() == 0)
+				continue;
+			String fileName = "";
+			//첨부파일 서버에 업로드
+			try {
+				fileName = UploadFileUtils.uploadFile(uploadPath, 
+						file.getOriginalFilename(), //파일명
+						file.getBytes()); //실제 파일 데이터
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+			System.out.println(fileName);
+			//첨부파일 객체를 생성
+			FileVO fileVo = new FileVO(file.getOriginalFilename(), fileName, 
+					bo_num);
+			//다오에게서 첨부파일 정보를 주면서 추가하라고 요청
+			boardDao.insertFile(fileVo);
+		}
+	}
+	
 	@Override
 	public ArrayList<BoardTypeVO> getBoardType(int authority) {
 		return boardDao.selectAllBoardType(authority);
@@ -40,41 +77,10 @@ public class BoardServiceImp implements BoardService {
 		//게시글 등록
 		boardDao.insertBoard(board);
 		
-		if(files == null || files.length == 0)
-			return true;
-		//반복문
-		for(MultipartFile file : files) {
-			if(file == null || file.getOriginalFilename().length() == 0)
-				continue;
-			String fileName = "";
-			//첨부파일 서버에 업로드
-			try {
-				fileName = UploadFileUtils.uploadFile(uploadPath, 
-						file.getOriginalFilename(), //파일명
-						file.getBytes()); //실제 파일 데이터
-			} catch (Exception e) {
-				e.printStackTrace();
-			} 
-			System.out.println(fileName);
-			//첨부파일 객체를 생성
-			FileVO fileVo = new FileVO(file.getOriginalFilename(), fileName, 
-					board.getBo_num());
-			//다오에게서 첨부파일 정보를 주면서 추가하라고 요청
-			boardDao.insertFile(fileVo);
-		}
+		uploadFiles(files, board.getBo_num());
 		return true;
 	}
-	private boolean checkBoard(BoardVO board) {
-		//게시글이 없거나, 게시글 제목이 비어있거나, 내용이 비어있으면
-		if(board == null || 
-			board.getBo_title() == null|| 
-			board.getBo_title().trim().length() == 0 ||
-			board.getBo_content() == null || 
-			board.getBo_content().trim().length() == 0)
-			return false;
-		
-		return true;
-	}
+	
 
 	@Override
 	public ArrayList<BoardVO> getBoardList() {
