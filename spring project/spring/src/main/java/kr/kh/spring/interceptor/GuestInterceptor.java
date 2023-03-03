b97fbda3758614fb50528eb67f2bacb0eca15658
stajun <stajun@naver.com>
2023-03-03 20:21:18 +0900

    스프링 17일차 : 회원 인터셉터/ 게스트 인터셉터 추가

---
+++ spring project/spring/src/main/java/kr/kh/spring/interceptor/GuestInterceptor.java
@@ -0,0 +1,26 @@
+package kr.kh.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.kh.spring.vo.MemberVO;
+
+public class GuestInterceptor extends HandlerInterceptorAdapter  {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		if(user != null) {
+			response.sendRedirect(request.getContextPath() + "/");
+			return false;
+		}
+		return true;
+	}
+}
