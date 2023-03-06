bf7b503e504c78291302135ea7e35b3532e28f03
stajun <stajun@naver.com>
2023-03-06 17:43:47 +0900

    web 기본프로젝트2

--- web/src/main/java/kr/kh/web/common/JDBCConnector.java
+++ web/src/main/java/kr/kh/web/common/JDBCConnector.java
@@ -1,33 +1,6 @@
 package kr.kh.web.common;
 
-<<<<<<< Updated upstream
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
-public class JDBCConnector {
-	private static Connection conn;
-	private JDBCConnector() {}
-	public static Connection getInstance() {
-		Properties props = new Properties();
-		String path = "C:\\Users\\staju\\eclipse-workspace\\web\\src\\main\\webapp\\properties\\database.properties";
-		FileInputStream fis;
-		try {
-			fis = new FileInputStream(path);
-			props.load(fis);
-			String driver = props.getProperty("driver").trim();
-			String url = props.getProperty("url").trim();
-			String username = props.getProperty("username").trim();
-			String password = props.getProperty("password").trim();
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url, username, password);
-			System.out.println("연경성공");
-			return conn;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-=======
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -47,22 +20,12 @@ public class JDBCConnector {
 			conn = DriverManager.getConnection(url, userName, password);
 			return conn;
 		}catch(Exception e) {
->>>>>>> Stashed changes
+
 			e.printStackTrace();
 		}
 		return null;
 	}
-<<<<<<< Updated upstream
-	public static void close() {
-		
-		if( conn != null ) {
-			try {
-				if( !conn.isClosed() ) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-=======
+
 	
 	public static void close() {
 		if(conn != null) {
@@ -71,7 +34,7 @@ public class JDBCConnector {
 					conn.close();
 				}
 			}catch(Exception e) {
->>>>>>> Stashed changes
+
 				e.printStackTrace();
 			}
 		}
--- web/src/main/java/kr/kh/web/controller/Main.java
+++ web/src/main/java/kr/kh/web/controller/Main.java
@@ -1,15 +1,12 @@
 package kr.kh.web.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import kr.kh.web.common.JDBCConnector;
 
 /**
  * Servlet implementation class Main
